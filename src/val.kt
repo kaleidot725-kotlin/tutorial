4d06d301455c1b3f146d0d1ea2992a57b160ddeb
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2017-11-19 22:36:35 +0900

    Update Kotlin助走読本 P1~P39

---
+++ src/val.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>): Unit {
+    val num : Int = 1
+    //num = 2                 // val cannot be reassinged
+
+    return Unit
+}
---
+++ src/var.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>): Unit {
+    var num : Int = 1
+    num = 2
+    println(num)
+    return Unit
+}
