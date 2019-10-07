59614451708b4bcd0f51aff1dd0039a1d502eac0
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2019-10-07 22:20:53 +0900

    Update inline noinline crossline test

--- src/function/higherorder.kt
+++ src/function/higherorder.kt
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.ReentrantLock
 
 fun main(args: Array<String>): Unit {
     val obj: Lock = ReentrantLock()
-    val number = lock<Int>(obj, ::getNumber)
+    val number = lockInlined<Int>(obj, ::getNumber)
     println("get number $number")
 
     return Unit
@@ -15,7 +15,7 @@ private fun getNumber () : Int {
     return 100
 }
 
-private fun <T> lock(lock: Lock, body: () -> T) : T {
+private fun <T> lockInlined(lock: Lock, body: () -> T) : T {
     lock.lock()
     try {
         return body()
